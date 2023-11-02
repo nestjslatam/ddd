b333437ec31e61218d2a394b61b4250aaa73e93e
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-11-02 08:05:50 -0500

    build: updating repository interfaces

--- libs/ddd/src/core/interfaces/domain-repository-write.interface.ts
+++ libs/ddd/src/core/interfaces/domain-repository-write.interface.ts
@@ -1,6 +1,6 @@
 export interface IDomainWriteRepository<TKey, TTable> {
   add(entity: TTable): Promise<void>;
-  addBatch(entity: TTable[]): Promise<void>;
+  addBatch(entities: TTable[]): Promise<void>;
   update(id: TKey, entity: TTable): Promise<void>;
   delete(id: TKey): Promise<void>;
 }
