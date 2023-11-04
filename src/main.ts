324ed5b3ab260f66c422b6020f6e9739d1ad4831
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-11-03 21:22:06 -0500

    build: adding ValidationPipe => ExceptionFactory

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { BadRequestException } from '@nestjs/common/exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalPipes(
+    //NOTE: How to generate custom error message object. Using exception factory
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      exceptionFactory: (errors) => {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+        return new BadRequestException(result);
+      },
+      stopAtFirstError: true,
+    }),
+  );
 
   app.enableShutdownHooks();
 
