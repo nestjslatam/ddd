389934a7ca7a4c2ce4402d813d38c582c225d536
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-12-08 20:08:13 -0500

    build: refactoring interface event publisher

--- libs/ddd/src/ddd-events/interfaces/domain-event-publisher.interface.ts
+++ libs/ddd/src/ddd-events/interfaces/domain-event-publisher.interface.ts
@@ -3,8 +3,12 @@ import { IDomainEvent } from './domain-event.interface';
 export interface IDomainEventPublisher<
   DomainEventBase extends IDomainEvent = IDomainEvent,
 > {
-  publish<T extends DomainEventBase = DomainEventBase>(domainEvent: T): any;
+  publish<T extends DomainEventBase = DomainEventBase>(
+    domainEvent: T,
+    context?: unknown,
+  ): any;
   publishAll?<T extends DomainEventBase = DomainEventBase>(
     domainEvents: T[],
+    context?: unknown,
   ): any;
 }
